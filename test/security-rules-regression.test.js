85836052e9fcca16284d74e1a935cab8b6a736ab
Yuuki Fukuda <yuuki.fukuda@dena.com>
2019-10-10 15:34:45 +0900

    testing

---
+++ test/security-rules-regression.test.js
@@ -0,0 +1,92 @@
+const firebase = require('@firebase/testing')
+
+const app = firebase.initializeTestApp({
+  projectId: 'firebase-emulators-test',
+  auth: null
+})
+
+test('成功', async () => {
+  const result = await app.firestore()
+    .collection('messages')
+    .add({
+      message: 'message',
+      createdAt: firebase.firestore.FieldValue.serverTimestamp()
+    })
+  expect(result).toBeInstanceOf(firebase.firestore.DocumentReference)
+})
+
+test('失敗: メッセージが空文字列', async () => {
+  try {
+    await app.firestore()
+      .collection('messages')
+      .add({
+        message: '',
+        createdAt: firebase.firestore.FieldValue.serverTimestamp()
+      })
+  } catch (error) {
+    expect(error.code).toEqual('permission-denied')
+  }
+})
+
+test('失敗: メッセージが長すぎる', async () => {
+  try {
+    await app.firestore()
+      .collection('messages')
+      .add({
+        message: '12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901',
+        createdAt: firebase.firestore.FieldValue.serverTimestamp()
+      })
+  } catch (error) {
+    expect(error.code).toEqual('permission-denied')
+  }
+})
+
+test('失敗: 適当な時刻', async () => {
+  try {
+    await app.firestore()
+      .collection('messages')
+      .add({
+        message: 'message',
+        createdAt: firebase.firestore.Timestamp.now()
+      })
+  } catch (error) {
+    expect(error.code).toEqual('permission-denied')
+  }
+})
+
+test('失敗: 不正なデータ形式', async () => {
+  expect.assertions(3)
+  try {
+    await app.firestore()
+      .collection('messages')
+      .add({
+        message: 'message',
+        createdAtAAA: firebase.firestore.FieldValue.serverTimestamp() // ここが間違っている
+      })
+  } catch (error) {
+    expect(error.code).toEqual('permission-denied')
+  }
+  try {
+    await app.firestore()
+      .collection('messages')
+      .add({
+        messageAAA: 'message', // ここが間違っている
+        createdAt: firebase.firestore.FieldValue.serverTimestamp()
+      })
+  } catch (error) {
+    expect(error.code).toEqual('permission-denied')
+  }
+  try {
+    await app.firestore()
+      .collection('messages')
+      .add({
+        message: 'message',
+        createdAt: firebase.firestore.Timestamp.now(),
+        AAA: '' // 余計なフィールド
+      })
+  } catch (error) {
+    expect(error.code).toEqual('permission-denied')
+  }
+})
+
+afterAll(async () => await app.delete())
